193f514f50df0221d4eb76b2b0f7cae1c0747997
matsufriends <pikapikan0526@gmail.com>
2024-07-15 10:02:35 +0900

    Preload対応

--- MornAspectGlobalSettings.cs
+++ MornAspectGlobalSettings.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MornAspect
 {
@@ -8,13 +12,21 @@ namespace MornAspect
     {
         [SerializeField] private Vector2 _resolution = new(1920, 1080);
         internal static MornAspectGlobalSettings Instance { get; private set; }
-
         internal Vector2 Resolution => _resolution;
 
         private void OnEnable()
         {
             Instance = this;
             MornAspectUtil.Log("Global Settings Loaded");
+#if UNITY_EDITOR
+            var preloadedAssets = PlayerSettings.GetPreloadedAssets().ToList();
+            if (preloadedAssets.Contains(this) &&
+                preloadedAssets.Count(x => x is MornAspectGlobalSettings) == 1) return;
+            preloadedAssets.RemoveAll(x => x is MornAspectGlobalSettings);
+            preloadedAssets.Add(this);
+            PlayerSettings.SetPreloadedAssets(preloadedAssets.ToArray());
+            MornAspectUtil.Log("Global Settings Added to Preloaded Assets!");
+#endif
         }
 
         private void OnDisable()
